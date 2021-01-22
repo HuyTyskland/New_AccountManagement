d19bc5d7a665b5f1fc9e4143ec1295548183b676
Huy3.nguyen <huy3.nguyen@lge.com>
2021-01-22 16:20:33 +0700

    create User class and define a function

---
+++ User.cpp
@@ -0,0 +1,23 @@
+/*
+ * User.cpp
+ *
+ *  Created on: Jan 22, 2021
+ *      Author: huy3.nguyen
+ */
+
+#include<iostream>
+#include<string>
+#include "Account.h"
+#include "User.h"
+
+using namespace std;
+
+User::User() {
+	// TODO Auto-generated constructor stub
+	isUser = true;
+}
+
+void User::sendDeletionRequest()
+{
+	cout << "send deletion request" << endl;
+}
---
+++ User.h
@@ -0,0 +1,17 @@
+/*
+ * User.h
+ *
+ *  Created on: Jan 22, 2021
+ *      Author: huy3.nguyen
+ */
+
+#ifndef USER_H_
+#define USER_H_
+
+class User : public Account {
+public:
+	User();
+	void sendDeletionRequest();
+};
+
+#endif /* USER_H_ */
