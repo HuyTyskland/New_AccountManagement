8abed2c6b127def7232e4f52a6c86a8a319506fa
Huy3.nguyen <huy3.nguyen@lge.com>
2021-01-22 17:21:53 +0700

    create class FileHandler for managing data

---
+++ FileHandler.cpp
@@ -0,0 +1,14 @@
+/*
+ * FileHandler.cpp
+ *
+ *  Created on: Jan 22, 2021
+ *      Author: huy3.nguyen
+ */
+
+#include "FileHandler.h"
+
+FileHandler::FileHandler() {
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ FileHandler.h
@@ -0,0 +1,17 @@
+/*
+ * FileHandler.h
+ *
+ *  Created on: Jan 22, 2021
+ *      Author: huy3.nguyen
+ */
+
+#ifndef FILEHANDLER_H_
+#define FILEHANDLER_H_
+
+class FileHandler {
+
+public:
+	FileHandler();
+};
+
+#endif /* FILEHANDLER_H_ */
